5894751418c0c7412d1b3023ec7c578c22669a44
Jiri Svitak <jsvitak@jsvitak-nb.(none)>
2012-03-26 12:55:55 +0200

    Added parameter for passing job name from work item to handler.

--- src/main/java/org/jenkinsci/plugins/jbpm/JenkinsJobWorkItemHandler.java
+++ src/main/java/org/jenkinsci/plugins/jbpm/JenkinsJobWorkItemHandler.java
@@ -13,42 +13,46 @@ import org.drools.runtime.process.WorkItemManager;
 
 public class JenkinsJobWorkItemHandler implements WorkItemHandler {
 
-	public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+    public void executeWorkItem(WorkItem workItem,
+	    WorkItemManager workItemManager) {
 
-		
-		//((Run) hudson.model.Hudson.getInstance().getItem(itemName))).scheduleBuild();
-		//((AbstractBuild) (hudson.model.Hudson.getInstance().getItem(itemName))).
-		
-		String itemName = workItem.getName();
-	
-		WorkItemCause cause = new WorkItemCause();
-		
-		System.out.println("itemName: " + itemName);
-		
-		Hudson h = Hudson.getInstance();
-		AbstractProject ap = h.getItemByFullName(itemName, AbstractProject.class);
-		System.out.println("getItemByFullName: " + ap);
-		
-		TopLevelItem ap2 = h.getItem(itemName);
-		System.out.println("getItem: " + ap2);
-		
-		System.out.println("getItemMap" + h.getItemMap());
-		System.out.println("getItems" + h.getItems());
-		
-		/*
-		Future future = ap.scheduleBuild2(0, cause);
+	// ((Run)
+	// hudson.model.Hudson.getInstance().getItem(itemName))).scheduleBuild();
+	// ((AbstractBuild)
+	// (hudson.model.Hudson.getInstance().getItem(itemName))).
 
-		try {
-			future.wait();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		*/
-		
+	String itemName = workItem.getName();
+	String jobName = (String) workItem.getParameter("JobNameToLaunch");
+
+	WorkItemCause cause = new WorkItemCause();
+
+	System.out.println("jobName: " + jobName);
+
+	Hudson h = Hudson.getInstance();
+	AbstractProject ap = h
+		.getItemByFullName(jobName, AbstractProject.class);
+	System.out.println("getItemByFullName: " + ap);
+
+	TopLevelItem ap2 = h.getItem(jobName);
+	System.out.println("getItem: " + ap2);
+
+	System.out.println("getItemMap" + h.getItemMap());
+	System.out.println("getItems" + h.getItems());
+
+	Future future = ap.scheduleBuild2(0, cause);
+
+	synchronized (future) {
+	    try {
+		future.wait();
+	    } catch (InterruptedException e) {
+		e.printStackTrace();
+	    }
 	}
-		
-	public void abortWorkItem(WorkItem arg0, WorkItemManager arg1) {
-		
-	}
+
+    }
+
+    public void abortWorkItem(WorkItem arg0, WorkItemManager arg1) {
+
+    }
 
 }
