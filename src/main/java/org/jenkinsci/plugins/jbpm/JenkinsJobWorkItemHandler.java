9571b1521b593369a47336ebdd5b767e8a778b43
Jiri Svitak <svitak.jiri@gmail.com>
2012-06-21 13:15:47 +0200

    JenkinsJobWorkItemHandler behavior changed from synchronous to asynchronous. This enables multiple jobs in parallel gateway to be run concurrently (non-blocking).

--- src/main/java/org/jenkinsci/plugins/jbpm/JenkinsJobWorkItemHandler.java
+++ src/main/java/org/jenkinsci/plugins/jbpm/JenkinsJobWorkItemHandler.java
@@ -15,22 +15,15 @@
 
     You should have received a copy of the GNU General Public License
     along with jBPM workflow plugin.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 package org.jenkinsci.plugins.jbpm;
 
-import hudson.model.Action;
-import hudson.model.JobProperty;
-import hudson.model.ParameterValue;
 import hudson.model.Result;
 import hudson.model.AbstractProject;
 import hudson.model.Hudson;
-import hudson.model.ParametersAction;
-import hudson.model.StringParameterValue;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Future;
 
@@ -40,56 +33,69 @@ import org.drools.runtime.process.WorkItemManager;
 
 /**
  * 
- * Implements a handler for the JenkinsJob work item,
- * which launches jobs and waits for their completion.
+ * Implements a handler for the JenkinsJob work item, which launches jobs and
+ * waits for their completion.
  * 
  * @author Jiri Svitak
- *
+ * 
  */
 @SuppressWarnings("rawtypes")
 public class JenkinsJobWorkItemHandler implements WorkItemHandler {
-    
-    public void executeWorkItem(WorkItem workItem,
-            WorkItemManager workItemManager) {
+
+    public void executeWorkItem(final WorkItem workItem,
+	    final WorkItemManager workItemManager) {
+
+	new Thread(new Runnable() {
+
+	    public void run() {
+		
+		// extract input variables
+		String jobName = (String) workItem.getParameter("jobName");
+
+		Logger.log("Started job " + jobName);
+
+		// start new job specified by jobName
+		Hudson h = Hudson.getInstance();
+		AbstractProject ap = h.getItemByFullName(jobName,
+			AbstractProject.class);
+
+		// schedule a build and wait for completion
+		WorkItemCause cause = new WorkItemCause();
+
+		// scheduleBuild(5, cause, new ParametersAction(new
+		// StringParameterValue("someparameter", "somevalue")))
+		// List<ParameterValue> parameters = new
+		// ArrayList<ParameterValue>();
+		// parameters.add(new StringParameterValue("name", "value"));
+		// Action action = new ParametersAction(parameters);
+
+		Future future = ap.scheduleBuild2(0, cause);
+
+		Result result = null;
+		synchronized (future) {
+		    try {
+			future.wait();
+			result = ap.getBuilds().getLastBuild().getResult();
+		    } catch (InterruptedException e) {
+			result = Result.ABORTED;
+			Logger.log("Aborted job " + jobName
+				+ ", returned state ABORTED");
+			Logger.log(e.toString());
+		    }
+		}
+
+		Map<String, Object> workItemResults = new HashMap<String, Object>();
+		workItemResults.put("jobResult", result);
+		workItemManager.completeWorkItem(workItem.getId(),
+			workItemResults);
+
+		Logger.log("Completed job " + jobName + " with result "
+			+ result.toString());
+
+	    }
+	    
+	}).start();
 	
-	// extract input variables
-	String jobName = (String) workItem.getParameter("jobName");
-	
-        Logger.log("Started job " + jobName);
-                
-        // start new job specified by jobName
-        Hudson h = Hudson.getInstance();
-        AbstractProject ap = h
-                .getItemByFullName(jobName, AbstractProject.class);
-
-        // schedule a build and wait for completion
-        WorkItemCause cause = new WorkItemCause();
-
-        // scheduleBuild(5, cause, new ParametersAction(new StringParameterValue("someparameter", "somevalue")))
-        //List<ParameterValue> parameters = new ArrayList<ParameterValue>();
-        //parameters.add(new StringParameterValue("name", "value"));
-        //Action action = new ParametersAction(parameters);
-
-        Future future = ap.scheduleBuild2(0, cause);
-
-        Result result = null;
-        synchronized (future) {
-            try {
-                future.wait();
-                result = ap.getBuilds().getLastBuild().getResult();
-            } catch (InterruptedException e) {
-                result = Result.ABORTED;
-                Logger.log("Aborted job " + jobName + ", returned state ABORTED");
-        	Logger.log(e.toString());
-            }
-        }
-        
-        Map<String,Object> workItemResults = new HashMap<String,Object>();
-        workItemResults.put("jobResult", result);
-        workItemManager.completeWorkItem(workItem.getId(), workItemResults);
-
-        Logger.log("Completed job " + jobName + " with result " + result.toString());
-        
     }
 
     public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
