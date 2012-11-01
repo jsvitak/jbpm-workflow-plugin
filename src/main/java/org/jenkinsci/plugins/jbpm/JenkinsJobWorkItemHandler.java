ef495c7e9acd10a39d95be01132fbb1dd35ca399
Jiri Svitak <svitak.jiri@gmail.com>
2012-11-01 16:53:38 +0100

    Update README.md, added a link to Jenkins wiki page.

--- src/main/java/org/jenkinsci/plugins/jbpm/JenkinsJobWorkItemHandler.java
+++ src/main/java/org/jenkinsci/plugins/jbpm/JenkinsJobWorkItemHandler.java
@@ -1,26 +1,26 @@
 /*
-* The MIT License
-*
-* Copyright (c) 2012 Jiri Svitak
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*/
+ * The MIT License
+ *
+ * Copyright (c) 2012, Jiri Svitak
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 package org.jenkinsci.plugins.jbpm;
 
@@ -51,57 +51,55 @@ import org.drools.runtime.process.WorkItemManager;
 public class JenkinsJobWorkItemHandler implements WorkItemHandler {
 
     public void executeWorkItem(final WorkItem workItem,
-	    final WorkItemManager workItemManager) {
+            final WorkItemManager workItemManager) {
 
-	new Thread(new Runnable() {
+        new Thread(new Runnable() {
 
-	    public void run() {
-		
-		// extract input variables
-		String jobName = (String) workItem.getParameter("jobName");
+            public void run() {
 
-		Logger.log("Started job " + jobName);
+                String jobName = (String) workItem.getParameter("jobName");
+                Logger.log("Started job " + jobName);
 
-		// start new job specified by jobName
-		Hudson h = Hudson.getInstance();
-		AbstractProject ap = h.getItemByFullName(jobName,
-			AbstractProject.class);
+                Hudson h = Hudson.getInstance();
+                AbstractProject ap = h.getItemByFullName(jobName,
+                        AbstractProject.class);
 
-		// schedule a build and wait for completion
-		@SuppressWarnings("unchecked")
-		List<ParameterValue> parameters = (List<ParameterValue>) workItem.getParameter("jobParameters");
+                @SuppressWarnings("unchecked")
+                List<ParameterValue> parameters = (List<ParameterValue>) workItem
+                        .getParameter("jobParameters");
 
-		Future future; 
-		if (parameters != null)
-		    future = ap.scheduleBuild2(0, new WorkItemCause(), new ParametersAction(parameters));
-		else
-		    future = ap.scheduleBuild2(0, new WorkItemCause());
-			
-		Result result = null;
-		synchronized (future) {
-		    try {
-			future.wait();
-			result = ap.getBuilds().getLastBuild().getResult();
-		    } catch (InterruptedException e) {
-			result = Result.ABORTED;
-			Logger.log("Aborted job " + jobName
-				+ ", returned state ABORTED");
-			Logger.log(e.toString());
-		    }
-		}
+                Future future;
+                if (parameters != null) {
+                    future = ap.scheduleBuild2(0, new WorkItemCause(),
+                            new ParametersAction(parameters));
+                } else {
+                    future = ap.scheduleBuild2(0, new WorkItemCause());
+                }
+                Result result = null;
+                synchronized (future) {
+                    try {
+                        future.wait();
+                        result = ap.getBuilds().getLastBuild().getResult();
+                    } catch (InterruptedException e) {
+                        result = Result.ABORTED;
+                        Logger.log("Aborted job " + jobName
+                                + ", returned state ABORTED");
+                        Logger.log(e.toString());
+                    }
+                }
 
-		Map<String, Object> workItemResults = new HashMap<String, Object>();
-		workItemResults.put("jobResult", result);
-		workItemManager.completeWorkItem(workItem.getId(),
-			workItemResults);
+                Map<String, Object> workItemResults = new HashMap<String, Object>();
+                workItemResults.put("jobResult", result);
+                workItemManager.completeWorkItem(workItem.getId(),
+                        workItemResults);
 
-		Logger.log("Completed job " + jobName + " with result "
-			+ result.toString());
+                Logger.log("Completed job " + jobName + " with result "
+                        + result.toString());
 
-	    }
-	    
-	}).start();
-	
+            }
+
+        }).start();
+
     }
 
     public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
